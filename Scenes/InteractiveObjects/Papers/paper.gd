d9a8255fdfb19b4b5c47b91647df23c0ab8d440b
EugenyX_X <38291801+PistrunXvX@users.noreply.github.com>
2023-10-01 16:55:27 +0300

    Add work-papers

--- Scenes/InteractiveObjects/Papers/paper.gd
+++ Scenes/InteractiveObjects/Papers/paper.gd
@@ -13,3 +13,20 @@ func _on_button_button_up():
 func _on_button_button_down():
 	dragging = true
 	offset = get_local_mouse_position() - position
+
+static func set_client(object):
+	var client = {
+		surnameClient = $Surname,
+		nameClient = $Name,
+		carBrand = $carBrand,
+		carColor = $carColor,
+		carNumber = $carNumber,
+	}
+	
+	client.surnameClient.text = "Фамилия: {name}".format({"name":object.name})
+	client.nameClient.text = "Имя: {name}".format({"name":object.surname})
+	client.carBrand.text = "Марка: {name}".format({"name":object.carBrand})
+	client.carColor.text = "Цвет: {name}".format({"name":object.carColor})
+	client.carNumber.text = "Номер: {name}".format({"name":object.carNum})
+
+
